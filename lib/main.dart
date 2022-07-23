04bfb4a89bae15e6f39cbe190ddf6772ac442324
daheeamond <daheeyeu@gmail.com>
2022-07-23 12:48:00 +0900

    toggle button

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,56 @@
 import 'package:flutter/material.dart';
-import 'Toggle/screens/intro.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:
+      title: 'ToggleButton',
+      home: ToggleButtonsExample(),
+    );
+  }
+}
+
+class ToggleButtonsExample extends StatefulWidget {
+  @override
+  ToggleButtonsExampleState createState() => new ToggleButtonsExampleState();
+}
+
+class ToggleButtonsExampleState extends State<ToggleButtonsExample> {
+  List<bool> _isSelected = [false, true, false];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ToggleButton'),
+      ),
+      body: Center(
+        child:  ToggleButtons(
+          children: <Widget>[
+            Icon(Icons.bluetooth),
+            Icon(Icons.wifi),
+            Icon(Icons.flash_on),
+          ],
+          isSelected: _isSelected,
+          onPressed: (int index) {
+            setState(() {
+              _isSelected[index] = !_isSelected[index];
+            });
+          },
+          // region example 1
+          color: Colors.grey,
+          selectedColor: Colors.red,
+          fillColor: Colors.lightBlueAccent,
+          // endregion
+          // region example 2
+          borderColor: Colors.lightBlueAccent,
+          selectedBorderColor: Colors.red,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          // endregion
+        ),
+      ),
     );
   }
 }
